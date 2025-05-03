2803ec5de095c7784194e0299cad992d12e82b99
Josh Hayden <blindcollective@protonmail.com>
2025-05-03 16:10:19 -0400

    Cleaned up valgrind call with variables, changed path logic, and added comment to tell the user to run this script from the project root.

--- valgrind/run.sh
+++ valgrind/run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# This script should be run from the project root (`./valgrind/run.sh`).
+
+SUPP_FILE="valgrind/supp/valgrind.supp"
+LOG_FILE="valgrind/log/valgrind.log"
+EXEC="build/blinks-thinks"
+
 valgrind --leak-check=full --track-origins=yes \
-    --suppressions=./supp/valgrind.supp \
-    --log-file=./log/valgrind.log ../build/blinks-thinks
+    --suppressions="$SUPP_FILE" \
+    --log-file="$LOG_FILE" "$EXEC"
